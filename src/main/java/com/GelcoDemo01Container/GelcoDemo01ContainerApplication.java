860d5cce81d9bed8e25533a0cd4f8d9c7406a5d7
Fayrus Peña Coz <fayrus.pena@unmsm.edu.pe>
2026-07-05 17:52:44 -0500

    feat: init GelcoDemo01Container parent project

---
+++ src/main/java/com/GelcoDemo01Container/GelcoDemo01ContainerApplication.java
@@ -0,0 +1,13 @@
+package com.GelcoDemo01Container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelcoDemo01ContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GelcoDemo01ContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GelcoDemo01Container/GelcoDemo01ContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GelcoDemo01Container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GelcoDemo01ContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
