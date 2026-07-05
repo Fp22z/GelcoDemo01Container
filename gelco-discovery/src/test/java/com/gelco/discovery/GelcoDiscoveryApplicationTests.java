72a3dc0519645c719802e89062aaf9279daeaafb
Fayrus Peña Coz <fayrus.pena@unmsm.edu.pe>
2026-07-05 18:07:19 -0500

    feat(discovery): add eureka discovery server module

---
+++ gelco-discovery/src/test/java/com/gelco/discovery/GelcoDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelco.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GelcoDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
