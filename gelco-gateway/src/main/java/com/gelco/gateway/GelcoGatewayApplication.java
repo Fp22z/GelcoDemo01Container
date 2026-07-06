31f7b2ccba09fb83759cc679f5c95974b085e0b4
Fayrus Peña Coz <fayrus.pena@unmsm.edu.pe>
2026-07-05 19:42:19 -0500

    feat(gateway): add api gateway module with WebFlux routing

---
+++ gelco-gateway/src/main/java/com/gelco/gateway/GelcoGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.gelco.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GelcoGatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GelcoGatewayApplication.class, args);
+	}
+}
---
+++ gelco-gateway/src/test/java/com/gelco/gateway/GelcoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gelco.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GelcoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
